a85df02994fa4c2e9a2b1f14242884a1a84ca1ed
Jyothis <jyothis.kp@simat.ac.in>
2021-09-14 02:13:41 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,165 @@
+
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['signin']))
+{
+$uname=$_POST['username'];
+$password=md5($_POST['password']);
+$sql ="SELECT EmpId,Password,Status,dept_code,lv_casual FROM tblemployees WHERE EmpId=:uname and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+ foreach ($results as $result) {
+    $status=$result->Status;
+    $_SESSION['eid']=$result->EmpId;
+    $_SESSION['deptcode']=$result->dept_code;
+    $_SESSION['lvcasualcount']=$result->lv_casual;
+    
+  } 
+if($status==0)
+{
+$msg="Your account is Inactive. Please contact admin";
+} else{
+$_SESSION['emplogin']=$_POST['username'];
+
+echo "<script type='text/javascript'> document.location = 'myprofile.php'; </script>";
+} }
+
+else{
+  echo "<script>alert('Invalid Details');</script>";
+  
+
+}
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIMAT</title>
+    <link rel="shortcut icon" href="assets/images/logo.jpeg" type="image/ico" />
+        
+        <!-- Title -->
+        <title>SIMAT LMS</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Responsive Admin Dashboard Template" />
+        <meta name="keywords" content="admin,dashboard" />
+        <meta name="author" content="Steelcoders" />
+        
+        <!-- Styles -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@600&display=swap" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css"/>
+             <link href="assets/css/materialdesign.css" rel="stylesheet">
+        <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">        
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        	
+        <!-- Theme Styles -->
+        <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
+        
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="http://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="http://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        
+    </head>
+    <body>
+        <div class="loader-bg"></div>
+       
+        <div class="mn-content fixed-sidebar">
+            <header class="mn-header navbar-fixed">
+                <nav style= "background-image: linear-gradient(to right, rgba(43, 0, 0), rgba(0, 16, 79))" class="cyan darken-1">
+                    <div class="nav-wrapper row">
+                        <section class="material-design-hamburger navigation-toggle">
+                            <a href="#" data-activates="slide-out" class="button-collapse show-on-large material-design-hamburger__icon">
+                                <span class="material-design-hamburger__layer"></span>
+                            </a>
+                        </section>
+                        <div class="header-title col s4">      
+                            <span class="chapter-title">SIMAT LMS | Faculty Leave Management System</span>
+                        </div>
+                        <div ><img class="sreeku"  style="
+                        width: 55px;
+                        height:50px;
+                        margin-bottom:20px;
+                        margin-top:5px;
+                         float:right; "
+                         src="assets/images/half.png">
+                        </div>
+                       
+                           
+                        </form>
+                     
+                        
+                    </div>
+                </nav>
+            </header>
+           
+           
+            <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                   
+                
+             
+          <div class="footer">
+                    <p class="copyright"><a href="simat.ac.in">SIMAT E-Governance</a>©</p>
+                
+                </div>
+                </div>
+            </aside>
+            <main  class="mn-inner">
+                <div class="row">
+                    <div class="col s12">
+                        <!-- <div class="page-title"><h4>Faculty Leave Management System</h4></div> -->
+
+                          <div  class="col s12 m6 l8 offset-l2 offset-m3">
+                              <div class="card white darken-1">
+
+                                  <div class="card-content ">
+                                 
+                                      <h4 style="text-align: center;color:#002d75;font-family: 'Rubik', sans-serif;" >WELCOME</h4> 
+                                      <h5 style="text-align: center;color:#002d75;font-family: 'Rubik', sans-serif;" >SREEPATHY E-GOVERNANCE SYSTEM</h5> <br> 
+                                        
+                                         <h4 style="text-align: center;"><button   onclick="window.location.href = './facindex.php';" type="button" class="btn btn-primary">Department Login</button></h4>  
+                                         <h4 style="text-align: center; "><button   onclick="window.location.href = './studentModule/index.php';" style="padding-left:45px;padding-right:45px" type="button" class="btn btn-primary">  Student Login<h4>........</h4>   </button></h4>
+
+                                          
+                                           
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                    </div>
+                </div>
+            </main>
+            
+        </div>
+        <div class="left-sidebar-hover"></div>
+        
+        <!-- Javascripts -->
+        <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+        <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+        <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+        <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+        <script src="assets/js/alpha.min.js"></script>
+        
+    </body>
+</html>
