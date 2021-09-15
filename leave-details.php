9513848e417fab3eef348ab43fc5eab966779cb3
ashone77 <aswinvharidas@gmail.com>
2021-09-15 10:56:00 +0530

    Spelling, typo fix

--- leave-details.php
+++ leave-details.php
@@ -71,7 +71,7 @@ $msg="Leave updated Successfully";
     <head>
         
         <!-- Title -->
-        <title>HR | Leave Details </title>
+        <title>Faculty | Leave Details </title>
         <link rel="shortcut icon" href="./assets/images/logo.jpeg" type="image/ico" />
         
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
