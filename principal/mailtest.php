06d7c6fdde832c28a2f27148a69126a446e99068
ashone77 <aswinvharidas@gmail.com>
2021-10-01 12:45:16 +0530

    php mailer

--- principal/mailtest.php
+++ principal/mailtest.php
@@ -11,13 +11,21 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    // $mail->isSMTP();                                            //Send using SMTP
+    // $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    // $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    // $mail->Username   = 'simatlms5@gmail.com';                     //SMTP username
+    // $mail->Password   = '#Simat@LMS100%';                               //SMTP password
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    // $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Host       = 'localhost';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = false;                                   //Enable SMTP authentication
     $mail->Username   = 'simatlms5@gmail.com';                     //SMTP username
     $mail->Password   = '#Simat@LMS100%';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 25; 
 
     //Recipients
     $mail->setFrom('simatlms5@gmail.com', 'E-Governance');
