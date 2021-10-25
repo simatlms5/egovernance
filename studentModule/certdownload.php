cde8ad269813d1bed1ea3cb8c0021950213bcece
Aswin Haridas <aswinvharidas@gmail.com>
2021-10-25 23:20:02 +0530

    Student link added

---
+++ studentModule/certdownload.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include('includes/config.php'); 
+$docno = $_GET['docno'];
+$sql = "SELECT * FROM bonafide_cert WHERE DocumentNumber=:docno";
+$query = $dbh->prepare($sql);
+$query->bindParam(':admno',$admno,PDO::PARAM_STR);
+$query->bindParam(':docno',$docno,PDO::PARAM_STR);
+$query->execute();  
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+    foreach($results as $result)
+    {
+         header('Content-Type:'.$result->mime);
+         header("Content-Disposition: attatchment; filename=bonafide-cert.pdf");
+         echo $result->pdf_file;
+    }
+}
